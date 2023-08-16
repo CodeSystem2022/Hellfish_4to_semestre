c25b86850d8e15f9ae263f6f54d666830b3030a1
Ana Ascua <83845164+aniascua@users.noreply.github.com>
2023-08-16 18:31:15 -0300

    agrego código a products.js

---
+++ JavaScript/e-commerce2022/client/js/products.js
@@ -0,0 +1,58 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 480,
+        quantity: 1,
+        img: "/e-commerce/client/media/bananas.png",
+    },
+    {
+        id: 2,
+        productName: "Hamburguesas",
+        price: 1200,
+        quantity: 1,
+        img: "/e-commerce/client/media/hamburguesas.png",
+    },
+    {
+        id: 3,
+        productName: "Facturas",
+        price: 1100,
+        quantity: 1,
+        img: "/e-commerce/client/media/facturas.jpg",
+    },
+    {
+        id: 4,
+        productName: "Mayonesa",
+        price: 800,
+        quantity: 2,
+        img: "/e-commerce/client/media/Aderezo-Mayonesa-Natura-475-Gr-1-9501.webp",
+    },
+    {
+        id: 5,
+        productName: "Gomitas",
+        price: 380,
+        quantity: 1,
+        img: "/e-commerce/client/media/gomitas.png",
+    },
+    {
+        id: 6,
+        productName: "Nesquik",
+        price: 690,
+        quantity: 10,
+        img: "/e-commerce/client/media/nesquik.png",
+    },
+    {
+        id: 7,
+        productName: "Pan de panchos",
+        price: 540,
+        quantity: 1,
+        img: "/e-commerce/client/media/pan-de-panchos.webp",
+    },
+    {
+        id: 8,
+        productName: "Vino",
+        price: 1500,
+        quantity: 1,
+        img: "/e-commerce/client/media/vino.jpg"
+    }
+]
