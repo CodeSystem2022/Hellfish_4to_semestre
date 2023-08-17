1d854f4a5612d21cd5c31937aae80eb594e91f31
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-08-17 00:42:54 -0300

    Tercer Semestre

---
+++ Java/estudiante/src/main/java/UTN/Main.java
@@ -0,0 +1,17 @@
+package UTN;
+
+import UTN.conexion.Conexion;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var conexion = Conexion.getConnection();
+
+        if (conexion != null) {
+            System.out.println("Conexión exitosa: " + conexion);
+
+        } else {
+            System.out.println("Hubo un error al conectarse");
+        }
+    }
+}
