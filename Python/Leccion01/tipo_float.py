b64aee783fba10a7e2ec27db1055bcbd0c216632
elfabri <111920054+elfabri@users.noreply.github.com>
2023-09-23 21:01:50 -0300

    Phyton 2.4 Exponencial negativo y positivo #46

--- Python/Leccion01/tipo_float.py
+++ Python/Leccion01/tipo_float.py
@@ -8,4 +8,12 @@ print(f"a: {a:.2f}")
 
 # Notacion exponencial (positivos negativos)
 a = 3e5
-print(f"a: {a}")
+print(f"a: {a:.2f}")
+
+a = 3e-5
+print(f"a: {a:.5f}")
+
+# Cualquier cálculo que incluya a un float, resulta en un tipo float
+a = 4.0 + 5
+print(a)
+print(type(a))
