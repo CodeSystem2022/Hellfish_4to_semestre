621558e5154bd109442c4496b5b5d7487ee28ae4
elfabri <111920054+elfabri@users.noreply.github.com>
2023-09-23 21:00:38 -0300

    Python 2.3 Tipo float como constructor y comienzo de exponencial #45

---
+++ Python/Leccion01/tipo_float.py
@@ -0,0 +1,11 @@
+# Profundizando en tipo float
+a = 3.0
+
+# float() puede recibir str e int
+a = float(10)
+a = float("10")
+print(f"a: {a:.2f}")
+
+# Notacion exponencial (positivos negativos)
+a = 3e5
+print(f"a: {a}")
