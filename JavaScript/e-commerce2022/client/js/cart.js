115c0e93d55275dce7f90e37fc2a860a9682b007
SofiaaSottile18 <112595421+SofiaaSottile18@users.noreply.github.com>
2023-09-28 16:33:01 -0300

    3.1 Avanzamos en el Body - cart js #62

--- JavaScript/e-commerce2022/client/js/cart.js
+++ JavaScript/e-commerce2022/client/js/cart.js
@@ -7,7 +7,8 @@ const displayCart = () => {
     modalContainer.innerHTML = "";
     modalContainer.style.display = "block";
     modalOverlay.style.display = "block";
-    //modal header
+    
+    // Modal header
     const modalHeader = document.createElement("div");
 
     const modalClose = document.createElement("div");
@@ -26,6 +27,28 @@ const displayCart = () => {
     modalHeader.append(modalTitle);
 
     modalContainer.append(modalHeader);
+    
+    // Modal body
+    cart.forEach((p) => {
+        const mB = document.createElement("div");
+        mB.className = "modal-body";
+        mB.innerHTML = `
+        <div class="product">
+            <img class="product-img" src="${p.img}">
+            <div class="product-info">
+                <h4>${p.productName}</h4>
+            </div>
+            <div class="quantity">
+                <span class="quantity-btn-decrease">-</span>
+                <span class="quantity-input">${p.quantity}</span>
+                <span class="quantity-btn-increase">+</span>
+            </div>
+            <div class="price">$ ${p.price * p.quantity}</div>
+            <div class="delete-product">X</div>
+        </div>
+        `;
+        modalContainer.append(modalBody);
+    })
 };
 
 cartBtn.addEventListener("click",displayCart);
