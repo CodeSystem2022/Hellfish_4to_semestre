371ac7f410c49e305d48db60bd5d44e791d0b9cd
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-10-10 18:04:14 -0300

    Node - 6.2 Pruebas para aclarar conceptos #95

--- Node/01-Hola-Mundo/entorno.js
+++ Node/01-Hola-Mundo/entorno.js
@@ -1,6 +1,14 @@
-let nombre = process.env.NOMBRE;
+let nombre = process.env.NOMBRE || "Sin nombre";
+let web = process.env.web || "Sin web";
 
 // para introducir esa variable de entorno al ejecutar
 // $env:NOMBRE="Carlos"; node entorno
 
+// Si no se establece, será undefined, a menos que || "a"
+
+// El valor se guarda como en una memoria cache
+// por lo que solo es necesario declarar estas variables de
+// esa forma, solo una vez
+
 console.log("Hola " + nombre);
+console.log("web: " + web);
