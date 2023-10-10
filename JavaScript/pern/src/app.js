47b8ee99a975b3cc6b359e62869e8cd8924be3a7
elfabri <navarro.fabricio.nahuel@gmail.com>
2023-10-10 17:50:01 -0300

    Js - 5.7 Lanzamiento del servidor -> Parte 1 y 2 #92

--- JavaScript/pern/src/app.js
+++ JavaScript/pern/src/app.js
@@ -4,7 +4,16 @@ import morgan from "morgan";
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.json({message: "Bienvenidos al proyecto"}));
+
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        status: "error",
+        message: err.message
+    })
+})
 
 export default app;
