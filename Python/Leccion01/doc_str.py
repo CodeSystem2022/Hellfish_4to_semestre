57a17a9ed4486b1c423357962f267b1506b28af4
FrancoFB <112664039+FrancoFB@users.noreply.github.com>
2023-10-14 21:51:25 -0300

    dunder doc_str.py

--- Python/Leccion01/doc_str.py
+++ Python/Leccion01/doc_str.py
@@ -1,9 +1,9 @@
+from mi_clase import MiClase
 
+#help(MiClase)
 
-'''
-Podemos crear documentación
-'''
-
-"""
-estas son las maneras de documentar
-"""
+#print(MiClase.__doc__)
+#print(MiClase.__init__.__doc__)
+#print(MiClase.mi_metodo.__doc__)
+print(MiClase.mi_metodo)
+print(type(MiClase.mi_metodo))
