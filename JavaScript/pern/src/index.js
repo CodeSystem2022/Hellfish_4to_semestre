0ebcba8d8c2c6ba0a1c824755b0e26c8e7531974
Franco Blanco <112664039+FrancoFB@users.noreply.github.com>
2023-11-07 20:44:50 -0300

    Update index.js

--- JavaScript/pern/src/index.js
+++ JavaScript/pern/src/index.js
@@ -1,10 +1,5 @@
 import app from "./app.js";
-import { pool } from "./db.js";
 
-pool.query("SELECT NOW()", (err, res) => {
-    console.log(err, res.rows);
+app.listen(3000);
 
-    app.listen(3000);
-    console.log("Server on port", 3000);
-    pool.end();
-});
+console.log("Server on port", 3000);
